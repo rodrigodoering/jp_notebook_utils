00e71def53e0ca2736bb4296427dcb073e1fde6d
rodrigodoering <34097975+rodrigodoering@users.noreply.github.com>
2021-06-05 12:30:24 -0300

    Add files via upload

--- Visuals/LaTex.py
+++ Visuals/LaTex.py
@@ -17,90 +17,133 @@ import numpy as np
 import pandas as pd
 
 # Importa funções específicas
-from sklearn.metrics import accuracy_score
 from IPython.display import display, Markdown
 
-
-# FUNÇÃO: numpy_convert 
-# Função para testar (e converter se necessário) listas de python em arrays numpy
-def numpy_convert(array):
-    '''
-    Argumentos:
-    ----------
-    array - lista de valores a ser validada
-    '''
-    if isinstance(array, list):
-        array = np.array(array)
-    return array
+from Visuals._utils_._type_definitions import *
+import Visuals._utils_._functions as utils
 
 
-# FUNÇÃO: number_to_string 
-# Converte um array em uma string compatível com a linguagem Latex
-def number_to_string(array):
-    '''
-    Argumentos:
-    ----------
-    array: numpy.array contendo os vetores que será convertido em string
-    '''      
-    rounded = (round(x,4) for x in array)
-    if array.dtype == np.float64:
-        # retorna as duas primeiras casas decimais se forem valores decimais
-        return ('%.2f' % val if str(val)[-2:] != '.0' else '%d' % val for val in rounded)
-    else:
-        return map(str, rounded)
+# Define argumentos personalisáveis do código LaTex
+# Armazenados em listas
+# O índice posicional de cada elemento será o argumento a ser passado nas funções
+
+# Lista de tamanhos disponíveis
+available_sizes = [
+    '$\\tiny ',
+    '$\\scriptsize ',
+    '$\\small ',
+    '$\\normalsize ',
+    '$\\large ',
+    '$\\Large ',
+    '$\\LARGE ',
+    '$\\huge ',
+    '$\\Huge ',
+]
+
+# Lista de estilos disponíveis
+available_styles = [
+    ' ',
+    ' \\displaystyle ',
+    ' \\textstyle ', 
+    ' \\scriptstyle ', 
+    ' \\scriptscriptstyle '
+]
+
+# Lista de fontes disponíveis
+available_fonts = [
+    '',
+    '\\mathcal',
+    '\\mathfrak',
+    '\\mathbb',
+    '\\mathrm',
+    '\\mathit',
+    '\\mathbf',
+    '\\mathsf',
+    '\\mathtt',      
+]
 
 
-def display_expression(expr):
-    '''
-    Argumentos:
-    ----------
-    expr: string - expressão em LaTex para ser exibida
-    ''' 
-    if not isinstance(expr, str):
+def display_expression(expr: str, size: int=4, style: int=0, font: int=0, spacing: int=1) -> NoReturn:
+    if type(expr) is not str:
         raise('Passe apenas strings')
-    display(Markdown(expr))
-
     
-# FUNÇÃO: display_vec
-# Printa no Stdout um vetor através de linguagem latex de visualização
-def display_vec(V, label=False):
+    if size > len(available_sizes) - 1:
+        print('**display_expression: Tamanho de fonte não disponível, setando tamanho default (4)')
+        size = 4
+
+    if style > len(available_styles) - 1:
+        print('**display_expression: Estilo de sentença não disponível, setando estilo default (0)')
+        style = 0
+    
+    if font > len(available_fonts) - 1:
+        print('**display_expression: Fonte não disponível, sentado fonte default (0) ')
+    
+    expr = ''.join([available_fonts[font], '{', expr, '}']) 
+    start = available_sizes[size] + available_styles[style]
+    space = '<br>' * spacing
+    final_expr = ''.join([space, start, expr, '$', space])
+    display(Markdown(final_expr))
+
+
+
+def display_vec(V: NumericArray, label: str=None, info: bool=True, **kwargs) -> NoReturn:
     '''
+    Exibe no stdout um vetor através de linguagem latex de visualização
+    
     Argumentos
     ----------
     V - numpy.array contendo o vetor a ser exibido com Latex
     label - rótulo do vetor, também será exibido em Latex
+    info - se verdadeira, exibe o espaço vetorial pertencente
+    **kwargs - a serem passados para display_expression()
     '''
-    V = numpy_convert(V)
+    V = utils.numpy_convert(V)
     
     if V.ndim > 1:
         V = V.reshape(-1)
         
-    if label:
-        latex_code = '<br>${} = '.format(label) + '\\begin{bmatrix}%s\\end{bmatrix}$<br>'
+    if label is not None:
+        vector_name = label
+        latex_code = '{} = '.format(label) + '\\begin{bmatrix}%s\\end{bmatrix}'
     else:
-        latex_code = '<br>$\\begin{bmatrix}%s\\end{bmatrix}$<br>'
+        vector_name = '{\\small \\text{vector space in }}'
+        latex_code = '\\begin{bmatrix}%s\\end{bmatrix}'
         
     # cria a string latex final contendo os valores do vetor
-    add = '\\\\'.join(number_to_string(V))
-    display_expression(latex_code % add)
+    add = '\\\\'.join(utils.number_to_string(V))
+    display_expression(latex_code % add, **kwargs)
+    
+    if info:
+        display_expression('\\text{Vector space in }\mathbb{R}^%d' % len(V), size=2)
 
 
 
-# FUNÇÃO: display_matrix
-# Printa no Stdout um vetor através de linguagem latex de visualização
-def display_matrix(M, n_rows=None, n_cols=None, label=False):
+def display_matrix(
+        M: NumericArray, 
+        n_rows: int=None, 
+        n_cols: int=None, 
+        label: str=None, 
+        info: bool=True, 
+        **kwargs
+        
+    ) -> NoReturn:
     '''
+    Exibe no Stdout a matriz numérica passada junto com demais argumentos
+    
     Argumentos:
     ----------
     M - Pandas.DataFrame ou 2-D Numpy.array contendo vetor ou matriz a ser plotada com markdown
     n_rows - quantidade máxima de linhas a serem exibidas
     n_cols - quantidade máxima de colunas a serem exibidas
+    label - nome/símbolo da matriz passada
+    info - se verdadeira, exibe dimensões da matriz com LaTex
+    **kwargs - a serem passados para display_expression()
     
     '''
-    if isinstance(M, pd.core.frame.DataFrame):
-        M = numpy_convert(M.values)
+    if isinstance(M, pd.DataFrame):
+        M = utils.numpy_convert(M.values)
     else:
-        M = numpy_convert(M)
+        M = utils.numpy_convert(M)
     
     # dimensões da matriz
     dims_matriz = M.shape
@@ -112,14 +155,20 @@ def display_matrix(M, n_rows=None, n_cols=None, label=False):
         M = M[:, :n_cols]
 
     # Gera o código latex
-    if label:
-        latex_code = '<br>${} = '.format(label) + '\\begin{bmatrix}%s\\end{bmatrix}$<br><br>'
+    if label is not None:
+        latex_code = '{} = '.format(label) + '\\begin{bmatrix}%s\\end{bmatrix}'
     else:
-        latex_code = '<br>$\\begin{bmatrix}%s\\end{bmatrix}$<br><br>'
+        latex_code = '\\begin{bmatrix}%s\\end{bmatrix}'
         
     # cria a string contendo os valores da matriz e exibe a matriz
-    M_str = (number_to_string(vec) for vec in M)
+    M_str = (utils.number_to_string(vec) for vec in M)
     add = '\\\\'.join('&'.join(vec) for vec in M_str)
-    display_expression(latex_code % add)
-    print('Dimensões da matriz: (%s x %s)' % (dims_matriz[0], dims_matriz[1]))
-    print()
+    display_expression(latex_code % add,  **kwargs)
+    
+    if info:
+        display_expression(
+            '\\text{Matrix of elements }a_{i,j} \in \\mathbb{R}^{%d \\times %d}' %(dims_matriz[0], dims_matriz[1]),
+            size=2
+        )
+
+
