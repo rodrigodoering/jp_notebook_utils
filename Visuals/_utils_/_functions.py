32411d7a1209c5f398aece88f0e0e531f65f7381
rodrigodoering <34097975+rodrigodoering@users.noreply.github.com>
2021-06-07 16:22:42 -0300

    Add files via upload

--- Visuals/_utils_/_functions.py
+++ Visuals/_utils_/_functions.py
@@ -6,8 +6,20 @@ Created on Wed May 12 19:56:10 2021
 """
 import numpy as np
 
+from Visuals._utils_._type_definitions import *
+
 
 def to_numpy(array: NumericArray) -> np.ndarray:
+    """
+    Avalia e converte se necessário um input numérico para um numpy.ndarray
+    
+    Argumentos:
+    ----------
+    array: array numérico que será testado
+
+    Retorna: Input convertido para um numpy.ndarray
+    -------    
+    """
     if not isinstance(array, np.ndarray):
 
         if any(type(array) is _type_ for _type_ in (list, tuple)):
@@ -29,11 +41,23 @@ def numpy_convert(
         squeeze: bool = False, 
         expand: bool = False,
         flat: bool = False
+        
     ) -> np.ndarray:
     '''
+    Recebe um input numérico e avalia se este é uma instancia do numpy. Permite algumas manipulações com o input
+    
     Argumentos:
     ----------
-    array: numpy.array contendo os vetores que será convertido em string
+    array: array numérico que será testado
+    check: se verdadeiro, avalia se o input é uma instancia de numpy.ndarray chamando to_numpy()
+    squeeze: se verdadeiro, assume-se um input n-dimensional que deve ser reduzir a um array uni-dimensional
+    expand: se verdadeiro, oposto à squeeze, incrementa o array para objetos dimensionais maiores
+    flat: útil para matrizes numéricas multidimensionais como grids de valores, 
+          retorna uma versão matricial com um vetor coordenada por linha
+    
+    Retorna: Input convertido para um numpy.ndarray
+    -------
+    
     '''     
     if check:
         ndarray = to_numpy(array)
@@ -52,11 +76,18 @@ def numpy_convert(
 
 
 
-def number_to_string(array):
+def number_to_string(array: NumericArray) -> Iterable[str]:
     '''
+    Transforma um array de valores numéricos nativos em strings que podém ser encaixadas em uma
+    expressão de LaTex por exemplo, especialmente utilizada pelas funções do módulo Visuals.LaTex
+    
     Argumentos:
     ----------
-    array: numpy.array contendo os vetores que será convertido em string
+    array: array contendo os vetores que será convertido em string
+    
+    Retorna: Iterable de strings, no caso um generator por ser mais performático. No caso a função
+    -------  retorna o objeto gerador inteiro ao invés de implementar uma função geradora com a chave "yield".
+             **Essa decisão foi mais estética do que por qualquer outro motivo, embora isso possa ser alterado em um futuro próximo
     '''      
     rounded = (round(x,4) for x in array)
     if array.dtype == np.float64:
@@ -66,8 +97,4 @@ def number_to_string(array):
         return map(str, rounded)
 
 
-
-def flat_grid(grid):
-    flatten = [coords.ravel() for coords in grid]
-    return np.array(flatten).T
 
